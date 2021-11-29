09afa66fe353dea0c6f7578bde3af43ecee2d2be
Matheus Bastos <matheus.bastos@dellead.com>
2021-11-29 08:56:48 -0300

    Adding base initial project

---
+++ course-project/src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { ShoppingEditComponent } from './components/shopping/shopping-list/shopping-edit/shopping-edit.component';
+import { ShoppingListComponent } from './components/shopping/shopping-list/shopping-list.component';
+import { HeaderComponent } from './components/header/header.component';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { RecipesComponent } from './components/recipe/recipes/recipes.component';
+import { RecipeListComponent } from './components/recipe/recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './components/recipe/recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './components/recipe/recipe-list/recipe-item/recipe-item.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    RecipesComponent,
+    RecipeListComponent,
+    RecipeDetailComponent,
+    RecipeItemComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent,
+  ],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
