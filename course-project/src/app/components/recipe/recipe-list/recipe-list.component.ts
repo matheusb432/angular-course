0c363ea212fbdeff4d935029225cbf89c0f3f6cc
Matheus Bastos <matheus.bastos@dellead.com>
2021-11-30 15:17:18 -0300

    Adding initial components styling

--- course-project/src/app/components/recipe/recipe-list/recipe-list.component.ts
+++ course-project/src/app/components/recipe/recipe-list/recipe-list.component.ts
@@ -1,15 +1,31 @@
+import { Recipe } from './recipe.model';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.scss']
+  styleUrls: ['./recipe-list.component.scss'],
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      'Beef Recipe',
+      'recipe description',
+      'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg'
+    ),
+    new Recipe(
+      'Shrimp Recipe',
+      'recipe desc',
+      'https://upload.wikimedia.org/wikipedia/commons/3/39/Recipe.jpg'
+    ),
+    new Recipe(
+      'Dish Recipe',
+      'recipe ---',
+      'https://cdn.pixabay.com/photo/2014/12/21/23/28/recipe-575434_960_720.png'
+    ),
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- course-project/src/app/components/shopping/shopping-list/shopping-list.component.ts
+++ course-project/src/app/components/shopping/shopping-list/shopping-list.component.ts
@@ -1,15 +1,18 @@
+import { Ingredient } from './../../../shared/ingredient.model';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
-  styleUrls: ['./shopping-list.component.scss']
+  styleUrls: ['./shopping-list.component.scss'],
 })
 export class ShoppingListComponent implements OnInit {
+  ingredients: Ingredient[] = [
+    new Ingredient('Apples', 5),
+    new Ingredient('Pears', 10),
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
